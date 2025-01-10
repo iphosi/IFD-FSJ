a426aeefa4d44d93350dc620fba4630b9e18a59c
iphosi <phos.tum@gmail.com>
2025-01-10 11:52:35 +0800

    Update

--- src/get_data_visualization.py
+++ src/get_data_visualization.py
@@ -152,12 +152,22 @@ def ablation_3():
     
     
 def perplexity_0():
+    output_path = "Self-Instruct-FSJ/figures/perplexity_0.png"
     data_path_list = [
         "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V2/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_instruction_gpt2_ppl.jsonl",
-        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V0/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_gcg_s1024_subset_instruction_gpt2_ppl.jsonl",
         "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V5/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_subset_instruction_gpt2_ppl.jsonl",
         "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V6/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_subset_instruction_gpt2_ppl.jsonl",
-        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V7/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_subset_instruction_gpt2_ppl.jsonl"
+        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V7/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_subset_instruction_gpt2_ppl.jsonl",
+        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/I-FSJ/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_instruction_gpt2_ppl.jsonl",
+        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V0/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_gcg_s1024_subset_instruction_gpt2_ppl.jsonl"
+    ]
+    label_list = [
+        "Self-Instruct-FSJ (P = 0)",
+        "Self-Instruct-FSJ (P = 4)",
+        "Self-Instruct-FSJ (P = 8)",
+        "Self-Instruct-FSJ (P = 16)",
+        "I-FSJ",
+        "GCG"
     ]
     data_list = []
     
@@ -166,36 +176,46 @@ def perplexity_0():
         data = df["ppl"].apply(lambda p: p[1]).tolist()
         data_list.append(data)
     
-    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+    fig, ax = plt.subplots(1, 1, figsize=(16, 6))
 
     ax.boxplot(
         data_list,
         vert=False,
         patch_artist=True, 
-        boxprops=dict(facecolor='lightblue', color='blue'),
-        whiskerprops=dict(color='green'),
-        flierprops=dict(markerfacecolor='red', marker='o', markersize=6)
+        boxprops=dict(facecolor="lightblue", color="blue"),
+        whiskerprops=dict(color="green"),
+        flierprops=dict(markerfacecolor="red", marker="o", markersize=6)
     )
-    plt.axvline(x=max(data_list[0]), color='red', linestyle='--', linewidth=2)
+    plt.axvline(x=max(data_list[0]), color="red", linestyle="--", linewidth=2)
     
-    plt.xscale('log')
+    plt.xscale("log")
 
-    ax.set_title('Comparison of Three Datasets with Statistics')
-    ax.set_yticklabels([f'Data 1', f'Data 2', f'Data 3', f'Data 4', f'Data 5'])
-    ax.set_xlabel('Value')
-
-    ax.legend()
+    ax.set_title("Llama-2-7b-chat-hf")
+    ax.set_yticklabels(label_list)
+    ax.set_xlabel("Perplexity")
 
     plt.show()
+    
+    plt.savefig(output_path, format="png")
     
     
 def perplexity_1():
+    output_path = "Self-Instruct-FSJ/figures/perplexity_1.png"
     data_path_list = [
         "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V2/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_instruction_gpt2_ppl_w20_s20.jsonl",
-        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V0/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_gcg_s1024_subset_instruction_gpt2_ppl_w20_s20.jsonl",
         "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V5/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_subset_instruction_gpt2_ppl_w20_s20.jsonl",
         "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V6/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_subset_instruction_gpt2_ppl_w20_s20.jsonl",
-        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V7/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_subset_instruction_gpt2_ppl_w20_s20.jsonl"
+        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V7/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_subset_instruction_gpt2_ppl_w20_s20.jsonl",
+        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/I-FSJ/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_instruction_gpt2_ppl_w20_s20.jsonl",
+        "/home/dq/repos/Self-Instruct-FSJ/datasets/benchmarks/AdvBench-V0/llama2/w_chat_template/sys_msg_v0/harmful_behaviors_gcg_s1024_subset_instruction_gpt2_ppl_w20_s20.jsonl"
+    ]
+    label_list = [
+        "Self-Instruct-FSJ (P = 0)",
+        "Self-Instruct-FSJ (P = 4)",
+        "Self-Instruct-FSJ (P = 8)",
+        "Self-Instruct-FSJ (P= 16)",
+        "I-FSJ",
+        "GCG"
     ]
     data_list = []
     
@@ -204,27 +224,27 @@ def perplexity_1():
         data = df["ppl"].apply(lambda p: p[1]).tolist()
         data_list.append(data)
     
-    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+    fig, ax = plt.subplots(1, 1, figsize=(16, 6))
 
     ax.boxplot(
         data_list,
         vert=False,
         patch_artist=True, 
-        boxprops=dict(facecolor='lightblue', color='blue'),
-        whiskerprops=dict(color='green'),
-        flierprops=dict(markerfacecolor='red', marker='o', markersize=6)
+        boxprops=dict(facecolor="lightblue", color="blue"),
+        whiskerprops=dict(color="green"),
+        flierprops=dict(markerfacecolor="red", marker="o", markersize=6)
     )
-    plt.axvline(x=max(data_list[0]), color='red', linestyle='--', linewidth=2)
+    plt.axvline(x=max(data_list[0]), color="red", linestyle="--", linewidth=2)
     
-    plt.xscale('log')
+    plt.xscale("log")
 
-    ax.set_title('Comparison of Three Datasets with Statistics')
-    ax.set_yticklabels([f'Data 1', f'Data 2', f'Data 3', f'Data 4', f'Data 5'])
-    ax.set_xlabel('Value')
-
-    ax.legend()
+    ax.set_title("Llama-2-7b-chat-hf")
+    ax.set_yticklabels(label_list)
+    ax.set_xlabel("Windowed Perplexity")
 
     plt.show()
+    
+    plt.savefig(output_path, format="png")
 
 
 if __name__ == "__main__":
